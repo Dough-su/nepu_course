b78e7edf19c9c230d3ad79b49a747d2facc38060
Dough-su <1601071777@qq.com>
2023-05-17 07:35:21 +0800

    update version

--- lib/global.dart
+++ lib/global.dart
@@ -55,7 +55,7 @@ class Global {
   static TextEditingController jwc_verifycodeController =
       TextEditingController(text: '');
   //版本号(每次正式发布都要改，改成和数据库一样)
-  static String version = "126";
+  static String version = "127";
   //教务处学号
   static String jwc_xuehao = '';
   //教务处密码
